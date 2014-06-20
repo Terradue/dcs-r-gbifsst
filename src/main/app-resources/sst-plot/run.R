e33239b8978a0dc5823c63a1f819c781c4a63c5c
Fabrice Brito <fabrice.brito@terradue.com>
2014-06-20 15:59:21 +0200

    Update run.R

--- src/main/app-resources/sst-plot/run.R
+++ src/main/app-resources/sst-plot/run.R
@@ -26,6 +26,10 @@ wcs.template$value[wcs.template$param == "coverage"] <- netcdf.variable
 
 # get URLs from thredds
 #thredds.urls <- GetThreddsURL(thredds)
+url=c("http://data.nodc.noaa.gov/thredds/wcs/ghrsst/L4/GLOB/UKMO/OSTIA/2013/364/20131230-UKMO-L4HRfnd-GLOB-v01-fv02-OSTIA.nc.bz2", 
+  "http://data.nodc.noaa.gov/thredds/wcs/ghrsst/L4/GLOB/UKMO/OSTIA/2013/365/20131231-UKMO-L4HRfnd-GLOB-v01-fv02-OSTIA.nc.bz")
+  date=c("20131230", "20131231") 
+thredds.urls <- data.frame(url, date)
 
 # read the inputs coming from stdin
 f <- file("stdin")
