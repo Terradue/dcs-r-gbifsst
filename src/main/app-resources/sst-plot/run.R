7f01b7c8a396c5fe9738def79892b336b484d798
Fabrice Brito <fabrice.brito@terradue.com>
2014-06-20 15:35:44 +0200

    Update run.R

---
+++ src/main/app-resources/sst-plot/run.R
@@ -0,0 +1,63 @@
+#!/usr/bin/Rscript --vanilla --slave --quiet
+
+# load rciop library to access the developer cloud sandbox functions
+library("rciop")
+
+# load the application package when mvn installed it
+library(rGBIFSST, lib.loc="/application/share/R/library")
+
+# load any other R library required
+library("rgeos")
+
+# load the parametee values with rciop.getparam() function
+netcdf.variable <- rciop.getparam("netcdf.variable")
+thredds <- rciop.getparam("thredds")
+start.date <- rciop.getparam("startdate")
+end.date <- rciop.getparam("enddate")
+
+# add a log message
+rciop.log("DEBUG", paste("I'm running a job with parameter values:", netcdf.variable, thredds, start.date, end.date))
+
+wcs.template <- GetWCSTemplate()
+
+wcs.template$value[wcs.template$param == "service"] <- "WCS"
+wcs.template$value[wcs.template$param == "version"] <- "1.0.0"
+wcs.template$value[wcs.template$param == "request"] <- "GetCoverage"
+wcs.template$value[wcs.template$param == "format"] <- "NetCDF3"
+wcs.template$value[wcs.template$param == "coverage"] <- netcdf.variable
+
+# get URLs from thredds
+thredds.urls <- GetThreddsURL(thredds)
+
+# read the inputs coming from stdin
+f <- file("stdin")
+open(f)
+
+while(length(bbox <- readLines(f, n=1)) > 0) {
+  
+  rciop.log("INFO", paste("processing bbox:", bbox))
+  
+  wcs.template$value[wcs.template$param == "bbox"] <- bbox
+  
+  r.stack <- c()
+  idx <- c()
+  
+  for (i in 1:nrow(thredd.urls)) {
+  
+    r <- GetWCSCoverage(thredd.urls[i, url], wcs.template, by.ref=FALSE)
+    d <- r.stack <- c(r.stack, r)  
+    
+    # update the index
+    idx <- c(idx, format(as.Date(thredd.urls[i, date]), format="%Y-%m-%d"))
+  }
+  
+  my.stack <- setZ(stack(r.stack), idx)
+  names(my.stack) <- idx
+  
+  stack.mean <- cellStats(my.stack, 'mean')
+  names(stack.mean) <- idx
+  
+  # publish the any results done 
+  rciop.publish(paste(TMPDIR,"output", sep="/"), recursive=TRUE, metalink=FALSE)
+ 
+}
