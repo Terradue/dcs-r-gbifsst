c6cd5bb98634d5a47bff008b2a320bde5b545127
Fabrice Brito <fabrice.brito@terradue.com>
2014-06-23 14:56:01 +0200

    Update run.R

--- src/main/app-resources/sst-plot/run.R
+++ src/main/app-resources/sst-plot/run.R
@@ -27,12 +27,14 @@ wcs.template$value[wcs.template$param == "format"] <- "NetCDF3"
 wcs.template$value[wcs.template$param == "coverage"] <- netcdf.variable
 
 # get URLs from thredds
-#thredds.urls <- GetThreddsURL(thredds)
-url=c("http://data.nodc.noaa.gov/thredds/wcs/ghrsst/L4/GLOB/UKMO/OSTIA/2013/364/20131230-UKMO-L4HRfnd-GLOB-v01-fv02-OSTIA.nc.bz2", 
-  "http://data.nodc.noaa.gov/thredds/wcs/ghrsst/L4/GLOB/UKMO/OSTIA/2013/365/20131231-UKMO-L4HRfnd-GLOB-v01-fv02-OSTIA.nc.bz2")
-  date=c("20131230", "20131231") 
+thredds.urls <- GetThreddsURL(thredds)
 
-thredds.urls <- data.frame(url, date, stringsAsFactors=FALSE)
+# unit test
+#url=c("http://data.nodc.noaa.gov/thredds/wcs/ghrsst/L4/GLOB/UKMO/OSTIA/2013/364/20131230-UKMO-L4HRfnd-GLOB-v01-fv02-OSTIA.nc.bz2", 
+#  "http://data.nodc.noaa.gov/thredds/wcs/ghrsst/L4/GLOB/UKMO/OSTIA/2013/365/20131231-UKMO-L4HRfnd-GLOB-v01-fv02-OSTIA.nc.bz2")
+#  date=c("20131230", "20131231") 
+
+#thredds.urls <- data.frame(url, date, stringsAsFactors=FALSE)
 
 # read the inputs coming from stdin
 f <- file("stdin")
